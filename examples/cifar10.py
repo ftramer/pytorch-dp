c1362b139e68e2c4638d68f85936d0ef9b7dd2b5
Florian Tramer <florian.tramer@gmail.com>
2020-06-15 13:59:00 -0700

    CIFAR-10 ResNet18 example

---
+++ examples/cifar10.py
@@ -0,0 +1,495 @@
+#!/usr/bin/env python3
+
+"""
+Runs CIFAR-10 training with differential privacy.
+"""
+
+import argparse
+import os
+import random
+import shutil
+import time
+import torchvision
+import warnings
+
+import torch
+import torch.backends.cudnn as cudnn
+import torch.nn as nn
+import torch.nn.parallel
+import torch.optim
+import torch.utils.data
+import torch.utils.data.distributed
+import torch.utils.tensorboard as tensorboard
+import torchvision.models as models
+import torchvision.transforms as transforms
+from torchdp import PrivacyEngine, stats, utils
+
+
+# The following few lines, enable stats gathering about the run
+# 1. where the stats should be logged
+stats.set_global_summary_writer(tensorboard.SummaryWriter("/tmp/stat"))
+# 2. enable stats
+stats.add(
+    # stats about gradient norms aggregated for all layers
+    stats.Stat(stats.StatType.CLIPPING, "AllLayers", frequency=0.1),
+    # stats about gradient norms per layer
+    stats.Stat(stats.StatType.CLIPPING, "PerLayer", frequency=0.1),
+    # stats about clipping
+    stats.Stat(stats.StatType.CLIPPING, "ClippingStats", frequency=0.1),
+    # stats on training accuracy
+    stats.Stat(stats.StatType.TRAIN, "accuracy", frequency=0.01),
+    # stats on validation accuracy
+    stats.Stat(stats.StatType.TEST, "accuracy"),
+)
+
+# The following lines enable stat gathering for the clipping process
+# and set a default of per layer clipping for the Privacy Engine
+clipping = {"clip_per_layer": False, "enable_stat": True}
+
+parser = argparse.ArgumentParser(description="PyTorch CIFAR-10 DP Training")
+parser.add_argument(
+    "-j",
+    "--workers",
+    default=4,
+    type=int,
+    metavar="N",
+    help="number of data loading workers (default: 4)",
+)
+parser.add_argument(
+    "--epochs", default=100, type=int, metavar="N", help="number of total epochs to run"
+)
+parser.add_argument(
+    "--start-epoch",
+    default=0,
+    type=int,
+    metavar="N",
+    help="manual epoch number (useful on restarts)",
+)
+parser.add_argument(
+    "-b",
+    "--batch-size",
+    default=256,
+    type=int,
+    metavar="N",
+    help="mini-batch size (default: 256), this is the total "
+    "batch size of all GPUs on the current node when "
+    "using Data Parallel or Distributed Data Parallel",
+)
+parser.add_argument(
+    "-na",
+    "--n_accumulation_steps",
+    default=1,
+    type=int,
+    metavar="N",
+    help="number of mini-batches to accumulate into an effective batch for SGD",
+)
+parser.add_argument(
+    "--lr",
+    "--learning-rate",
+    default=0.01,
+    type=float,
+    metavar="LR",
+    help="initial learning rate",
+    dest="lr",
+)
+parser.add_argument("--momentum", default=0.9, type=float, metavar="M", help="momentum")
+parser.add_argument(
+    "--wd",
+    "--weight-decay",
+    default=1e-2,
+    type=float,
+    metavar="W",
+    help="weight decay (default: 1e-2)",
+    dest="weight_decay",
+)
+parser.add_argument(
+    "-p",
+    "--print-freq",
+    default=10,
+    type=int,
+    metavar="N",
+    help="print frequency (default: 10)",
+)
+parser.add_argument(
+    "--seed", default=None, type=int, help="seed for initializing training. "
+)
+parser.add_argument("--gpu", default=None, type=int, help="GPU id to use.")
+parser.add_argument(
+    "--sigma",
+    type=float,
+    default=1.0,
+    metavar="S",
+    help="Noise multiplier (default 1.0)",
+)
+parser.add_argument(
+    "-c",
+    "--max-per-sample-grad_norm",
+    type=float,
+    default=1.0,
+    metavar="C",
+    help="Clip per-sample gradients to this norm (default 1.0)",
+)
+parser.add_argument(
+    "--disable-dp",
+    action="store_true",
+    default=False,
+    help="Disable privacy training and just train with vanilla SGD",
+)
+parser.add_argument(
+    "--delta",
+    type=float,
+    default=1e-5,
+    metavar="D",
+    help="Target delta (default: 1e-5)",
+)
+parser.add_argument(
+    "--checkpoint-file",
+    type=str,
+    default="checkpoint",
+    help="path to save check points",
+)
+parser.add_argument(
+    "--resume",
+    default="",
+    type=str,
+    metavar="PATH",
+    help="path to latest checkpoint (default: none)",
+)
+parser.add_argument(
+    "-e",
+    "--evaluate",
+    dest="evaluate",
+    action="store_true",
+    help="evaluate model on validation set",
+)
+
+best_acc1 = 0
+
+
+def main():
+    global best_acc1
+    args = parser.parse_args()
+
+    if args.seed is not None:
+        random.seed(args.seed)
+        torch.manual_seed(args.seed)
+        cudnn.deterministic = True
+        warnings.warn(
+            "You have chosen to seed training. "
+            "This will turn on the CUDNN deterministic setting, "
+            "which can slow down your training considerably! "
+            "You may see unexpected behavior when restarting "
+            "from checkpoints."
+        )
+
+    if args.gpu is not None:
+        print("Use GPU: {} for training".format(args.gpu))
+
+    # create model: resnet 18
+    # since our differential privacy engine does not support BatchNormXd
+    # we need to replace all such blocks with DP-aware normalisation modules
+    model = models.resnet18(num_classes=10)
+    model = utils.convert_batchnorm_modules(model)
+    # model = utils.nullify_batchnorm_modules(model, None)
+
+    if args.gpu is not None:
+        torch.cuda.set_device(args.gpu)
+        model = model.cuda(args.gpu)
+    else:
+        model = torch.nn.DataParallel(model).cuda()
+
+    # define loss function (criterion) and optimizer
+    criterion = nn.CrossEntropyLoss().cuda(args.gpu)
+
+    optimizer = torch.optim.SGD(
+        model.parameters(),
+        args.lr,
+        momentum=args.momentum,
+        weight_decay=args.weight_decay,
+    )
+
+    # optionally resume from a checkpoint
+    if args.resume:
+        if os.path.isfile(args.resume):
+            print("=> loading checkpoint '{}'".format(args.resume))
+            if args.gpu is None:
+                checkpoint = torch.load(args.resume)
+            else:
+                # Map model to be loaded to specified single gpu.
+                loc = "cuda:{}".format(args.gpu)
+                checkpoint = torch.load(args.resume, map_location=loc)
+
+            args.start_epoch = checkpoint["epoch"]
+            best_acc1 = checkpoint["best_acc1"]
+            if args.gpu is not None:
+                # best_acc1 may be from a checkpoint from a different GPU
+                best_acc1 = best_acc1.to(args.gpu)
+            model.load_state_dict(checkpoint["state_dict"])
+            optimizer.load_state_dict(checkpoint["optimizer"])
+            print(
+                "=> loaded checkpoint '{}' (epoch {})".format(
+                    args.resume, checkpoint["epoch"]
+                )
+            )
+        else:
+            print("=> no checkpoint found at '{}'".format(args.resume))
+
+    cudnn.benchmark = True
+
+    mean = (0.4914, 0.4822, 0.4465)
+    std = (0.2023, 0.1994, 0.2010)
+
+    transform_train = transforms.Compose(
+        [
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(mean, std),
+        ]
+    )
+
+    transform_test = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize(mean, std),]
+    )
+
+    train_dataset = torchvision.datasets.CIFAR10(
+        root="./data", train=True, download=True, transform=transform_train
+    )
+    test_dataset = torchvision.datasets.CIFAR10(
+        root="./data", train=False, download=False, transform=transform_test
+    )
+
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=args.batch_size,
+        shuffle=True,
+        num_workers=args.workers,
+        pin_memory=True,
+    )
+
+    val_loader = torch.utils.data.DataLoader(
+        test_dataset,
+        batch_size=args.batch_size,
+        shuffle=False,
+        num_workers=args.workers,
+        pin_memory=True,
+    )
+
+    if not args.disable_dp:
+        print("PRIVACY ENGINE ON")
+        privacy_engine = PrivacyEngine(
+            model,
+            batch_size=args.batch_size * args.n_accumulation_steps,
+            sample_size=len(train_dataset),
+            alphas=[1 + x / 10.0 for x in range(1, 100)] + list(range(12, 64)),
+            noise_multiplier=args.sigma,
+            max_grad_norm=args.max_per_sample_grad_norm,
+            **clipping,
+        )
+        privacy_engine.attach(optimizer)
+    else:
+        print("PRIVACY ENGINE OFF")
+
+    if args.evaluate:
+        validate(val_loader, model, criterion, args)
+        return
+
+    for epoch in range(args.start_epoch, args.epochs + 1):
+        adjust_learning_rate(optimizer, epoch, args)
+
+        # train for one epoch
+        train(train_loader, model, criterion, optimizer, epoch, args)
+
+        # evaluate on validation set
+        acc1 = validate(val_loader, model, criterion, args)
+
+        # remember best acc@1 and save checkpoint
+        is_best = acc1 > best_acc1
+        best_acc1 = max(acc1, best_acc1)
+
+        save_checkpoint(
+            {
+                "epoch": epoch + 1,
+                "arch": "ResNet",
+                "state_dict": model.state_dict(),
+                "best_acc1": best_acc1,
+                "optimizer": optimizer.state_dict(),
+            },
+            is_best,
+            filename=args.checkpoint_file + ".tar",
+        )
+
+
+def train(train_loader, model, criterion, optimizer, epoch, args):
+    batch_time = AverageMeter("Time", ":6.3f")
+    data_time = AverageMeter("Data", ":6.3f")
+    losses = AverageMeter("Loss", ":.4e")
+    top1 = AverageMeter("Acc@1", ":6.2f")
+
+    # switch to train mode
+    model.train()
+
+    end = time.time()
+
+    for i, (images, target) in enumerate(train_loader):
+        # measure data loading time
+        data_time.update(time.time() - end)
+
+        if args.gpu is not None:
+            images = images.cuda(args.gpu, non_blocking=True)
+        target = target.cuda(args.gpu, non_blocking=True)
+
+        # compute output
+        output = model(images)
+        loss = criterion(output, target)
+
+        # measure accuracy and record loss
+        (acc1,) = accuracy(output, target)
+        stats.update(stats.StatType.TRAIN, acc1=acc1[0])
+        losses.update(loss.item(), images.size(0))
+        top1.update(acc1[0], images.size(0))
+
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+
+        if args.n_accumulation_steps > 1:
+            optimizer.virtual_step()
+
+        # make sure we take a step after processing the last mini-batch in the
+        # epoch to ensure we start the next epoch with a clean state
+        if ((i + 1) % args.n_accumulation_steps == 0) or ((i + 1) == len(train_loader)):
+            optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+    if not args.disable_dp:
+        epsilon, best_alpha = optimizer.privacy_engine.get_privacy_spent(args.delta)
+        print(
+            f"Train Epoch: {epoch:3d} \t"
+            f"Loss: {losses.avg:.6f} \t"
+            f"Top1: {top1.avg:.2f} "
+            f"(Ɛ = {epsilon:.3f}, 𝛿 = {args.delta}) for α = {best_alpha}"
+        )
+    else:
+        print(
+            f"Train Epoch: {epoch:3d} \t"
+            f"Loss: {losses.avg:.6f} \t"
+            f"Top1: {top1.avg:.2f}"
+        )
+
+
+def validate(val_loader, model, criterion, args):
+    batch_time = AverageMeter("Time", ":6.3f")
+    losses = AverageMeter("Loss", ":.4e")
+    top1 = AverageMeter("Acc@1", ":6.2f")
+
+    # switch to evaluate mode
+    model.eval()
+
+    with torch.no_grad():
+        end = time.time()
+        for i, (images, target) in enumerate(val_loader):
+            if args.gpu is not None:
+                images = images.cuda(args.gpu, non_blocking=True)
+            target = target.cuda(args.gpu, non_blocking=True)
+
+            # compute output
+            output = model(images)
+            loss = criterion(output, target)
+
+            # measure accuracy and record loss
+            (acc1,) = accuracy(output, target)
+            losses.update(loss.item(), images.size(0))
+            top1.update(acc1[0], images.size(0))
+
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+        print(f"Test:\t\t\t" f"Loss: {losses.avg:.6f} \t" f"Top1: {top1.avg:.2f}")
+
+    return top1.avg
+
+
+def save_checkpoint(state, is_best, filename="checkpoint.tar"):
+    torch.save(state, filename)
+    if is_best:
+        shutil.copyfile(filename, "model_best.pth.tar")
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self, name, fmt=":f"):
+        self.name = name
+        self.fmt = fmt
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        fmtstr = "{name} {val" + self.fmt + "} ({avg" + self.fmt + "})"
+        return fmtstr.format(**self.__dict__)
+
+
+class ProgressMeter(object):
+    def __init__(self, num_batches, meters, prefix=""):
+        self.batch_fmtstr = self._get_batch_fmtstr(num_batches)
+        self.meters = meters
+        self.prefix = prefix
+
+    def display(self, batch):
+        entries = [self.prefix + self.batch_fmtstr.format(batch)]
+        entries += [str(meter) for meter in self.meters]
+        print("\t".join(entries))
+
+    def _get_batch_fmtstr(self, num_batches):
+        num_digits = len(str(num_batches // 1))
+        fmt = "{:" + str(num_digits) + "d}"
+        return "[" + fmt + "/" + fmt.format(num_batches) + "]"
+
+
+def adjust_learning_rate(optimizer, epoch, args):
+    lr = args.lr
+
+    if epoch > 40:
+        lr /= 10
+    elif epoch > 80:
+        lr /= 100
+
+    for param_group in optimizer.param_groups:
+        param_group["lr"] = lr
+
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the accuracy over the k top predictions for the specified values of k"""
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res
+
+
+if __name__ == "__main__":
+    main()
